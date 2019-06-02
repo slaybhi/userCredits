637fa01e291f340c2afe5397b07079672b40fd07
slaybhi <a.abhijith1adoor@gmail.com>
2019-06-02 16:36:05 +0530

    individual user view added

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var express     = require("express"),
     mongoose    = require("mongoose");
 
 mongoose.connect("mongodb://localhost/users");
+var userSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    credit:Number
+});
+var users = mongoose.model("users",userSchema);
+// users.create({name:"Rawn",email:"a.abhijith1adoor@gmail.com",credit:50},function(err,users){
+//     if(err){console.log(err)}
+//     else{console.log(users)}
+// });
 
 //app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
@@ -12,19 +22,30 @@ app.get('/',function(req,res){
 });
 
 app.get('/user',function(req,res){                                      //list all the users
-    var users= [
 
-        {name:"Abhijth",email:"abc@gmail.com",credit:100},
-        {name:"Abhijth",email:"abc@gmail.com",credit:100},
-        {name:"Abhijth",email:"abc@gmail.com",credit:100},
-        {name:"Abhijth",email:"abc@gmail.com",credit:100},       
-        {name:"Abhijth",email:"abc@gmail.com",credit:100},
+    users.find({},function(err,users){
+        if(err){
+            console.log(err)
+        }
+        else   {
+            res.render("userlist",{users:users});
+        }
+    })
 
-];
-
-
-    res.render("userlist",{users:users});
+   
 });
+
+app.get("/user/:id",function(req,res){
+    users.findById(req.params.id,function(err,founduser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("show",{users:founduser});
+        }
+    })
+   
+})
 
 
 
